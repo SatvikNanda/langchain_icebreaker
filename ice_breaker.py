bd7c7accf48dc6e3f99485d47bdfb8a8bf55c6d8
satvik nanda <satvik.nanda@gmail.com>
2024-08-06 23:27:13 +0530

    first testing

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,28 +2,17 @@ from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
+
+from third_parties.linkedin import scrape_linkedin_profile
 import os
 
-information = """
-Elon Reeve Musk (/ˈiːlɒn/; born June 28, 1971) is a businessman and investor known for his key roles in space company SpaceX and 
-automotive company Tesla, Inc. Other involvements include ownership of X Corp., formerly Twitter, and his role in the founding of
-The Boring Company, xAI, Neuralink and OpenAI. He is one of the wealthiest people in the world; as of June 2024, Forbes estimates
-his net worth to be US$214 billion.[4]
 
-Musk was born in Pretoria to Maye and Errol Musk and briefly attended the University of Pretoria before immigrating to Canada at
-age 18, acquiring citizenship through his Canadian-born mother. Two years later, he matriculated at Queen's University at Kingston
-in Canada. Musk later transferred to the University of Pennsylvania and received bachelor's degrees in economics and physics.
-He moved to California in 1995 to attend Stanford University, but dropped out after two days and, with his brother Kimbal, 
-co-founded online city guide software company Zip2. The startup was acquired by Compaq for $307 million in 1999. That same 
-year, Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to form PayPal. In October 2002, eBay acquired
-PayPal for $1.5 billion. Using $100 million of the money he made from the sale of PayPal, Musk founded SpaceX, a spaceflight
-services company, in 2002"""
 
 if __name__ == "__main__":
     print("Hello Langchain")
 
     summary_template = """
-    given the information {information} about a person, I want you to create:
+    given the Linkedin information {information} about a person, I want you to create:
     1. a short summary
     2. two interesting facts about them
     """
@@ -34,6 +23,7 @@ if __name__ == "__main__":
     #llm = ChatOllama(model="llama3")
 
     chain = summary_prompt_template | llm | StrOutputParser()
+    linkedin_data = scrape_linkedin_profile(Linkedin_profile_url="https://www.linkedin.com/in/sanya-nanda-aba12218b/", mock=True)
 
-    res = chain.invoke(input={"information":information})
+    res = chain.invoke(input={"information":linkedin_data})
     print(res)
---
+++ third_parties/linkedin.py
@@ -0,0 +1,50 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def scrape_linkedin_profile(Linkedin_profile_url: str, mock: bool = False):
+    """Scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinedIn profile"""
+
+    if mock:
+        Linkedin_profile_url = "https://gist.githubusercontent.com/SatvikNanda/86474ecb5403f31f67a180a19df5e992/raw/ae3f154ee199aab5d13fcc8195eb9e503237d13a/sanya-nanda.json"
+        response = requests.get(
+            Linkedin_profile_url,
+            timeout=10,
+        )
+
+    else:
+        api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+        header_dic = {'Authorization': f'Bearer {os.environ.get("Proxy_API_KEY")}'}
+        
+        response = requests.get(
+                        api_endpoint,
+                        params={"url": Linkedin_profile_url},
+                        headers=header_dic,
+                        timeout=10,
+                        )
+        
+    data = response.json()
+    #compressing data variable
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+    return data
+
+
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            Linkedin_profile_url="https://www.linkedin.com/in/sanya-nanda-aba12218b/", mock=True
+        )
+    )
