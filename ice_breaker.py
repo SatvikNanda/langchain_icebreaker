85e14aadfd5548b4a8588d64b702ed9528475df4
satvik nanda <satvik.nanda@gmail.com>
2024-08-18 15:43:10 +0530

    integrated the agent in iceBreaker script

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,12 +4,13 @@ from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linked_lookup_agent import lookup as linkedin_lookup_agent
 import os
 
 
-
-if __name__ == "__main__":
-    print("Hello Langchain")
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(Linkedin_profile_url=linkedin_username, mock=True)
 
     summary_template = """
     given the Linkedin information {information} about a person, I want you to create:
@@ -23,7 +24,16 @@ if __name__ == "__main__":
     #llm = ChatOllama(model="llama3")
 
     chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(Linkedin_profile_url="https://www.linkedin.com/in/sanya-nanda-aba12218b/", mock=True)
+    #linkedin_data = scrape_linkedin_profile(Linkedin_profile_url="https://www.linkedin.com/in/sanya-nanda-aba12218b/", mock=True)
 
     res = chain.invoke(input={"information":linkedin_data})
     print(res)
+
+
+if __name__ == "__main__":
+    print("Ice breaker Enter")
+    ice_break_with(name="Satvik Nanda")
+
+    
+
+    
