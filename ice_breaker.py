ad20f495a050990f6b990b394ea410b7c9bc2165
satvik nanda <satvik.nanda@gmail.com>
2024-08-18 16:08:39 +0530

    created output parsers

--- ice_breaker.py
+++ ice_breaker.py
@@ -5,6 +5,7 @@ from langchain_core.output_parsers import StrOutputParser
 
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linked_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser
 import os
 
 
@@ -16,14 +17,16 @@ def ice_break_with(name: str) -> str:
     given the Linkedin information {information} about a person, I want you to create:
     1. a short summary
     2. two interesting facts about them
+    \n{format_instructions}
     """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template,
+                                             partial_variables={"format_instructions": summary_parser.get_format_instructions()})
 
     llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo", api_key=os.getenv("OPENAI_API_KEY"))
     #llm = ChatOllama(model="llama3")
 
-    chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
     #linkedin_data = scrape_linkedin_profile(Linkedin_profile_url="https://www.linkedin.com/in/sanya-nanda-aba12218b/", mock=True)
 
     res = chain.invoke(input={"information":linkedin_data})
